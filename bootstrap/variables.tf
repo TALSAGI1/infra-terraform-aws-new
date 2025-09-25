3b8bb6451cbab3a19f2a87421e33c4db1488ba4e
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-25 16:50:58 +0300

    Update variables.tf

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,3 +1,9 @@
-variable "region"         { type = string }
-variable "bucket_name"    { type = string }   # ייחודי גלובלית, למשל my-tfstate-<name>-prod-2025
-variable "dynamodb_table" { type = string }   # למשל tf-locks-<name>-prod
+variable "region"         { 
+type = string
+}
+variable "bucket_name"    {
+type = string
+}   # ייחודי גלובלית, למשל my-tfstate-<name>-prod-2025
+variable "dynamodb_table" { 
+type = string
+}   # למשל tf-locks-<name>-prod
