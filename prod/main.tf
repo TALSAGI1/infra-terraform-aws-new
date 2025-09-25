97c10070b7a169bcddded6b5971446eeac24c678
TALSAGI1 <talsagi3012@gmail.com>
2025-09-25 13:24:03 +0300

    remove broken/empty prod directory

--- prod/main.tf
+++
@@ -1,7 +0,0 @@
-module "infra" {
-  source         = "../modules/infra"   # עדכני אם המודול ממוקם אחרת
-  vpc_cidr       = var.vpc_cidr
-  public_subnets = var.public_subnets
-  instance_type  = var.instance_type
-  key_name       = var.key_name
-}
