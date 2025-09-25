f78d1817d7cd514c027345869f36f9f64ab95fe7
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-25 14:10:22 +0300

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -19,8 +19,12 @@ module "sg" {
 data "aws_ami" "al2023" {
   most_recent = true
   owners      = ["amazon"]
-  filter { name = "name"         values = ["al2023-ami-*-x86_64"] }
-  filter { name = "architecture" values = ["x86_64"] }
+  filter { name = "name"     
+values = ["al2023-ami-*-x86_64"] 
+}
+  filter { name = "architecture"
+values = ["x86_64"] 
+}
 }
 
 # --- EC2 ---
