429b617407c841c311c82d1deba66c86b14e8b44
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-25 14:17:36 +0300

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,13 +1,7 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.this.id
 }
 
-# אם את יוצרת כמה סאבטים ציבוריים במשאב aws_subnet.public (עם count/for_each):
 output "public_subnet_ids" {
   value = [for s in aws_subnet.public : s.id]
 }
-
-# אם יש רק סאבט ציבורי יחיד:
-# output "public_subnet_id" {
-#   value = aws_subnet.public.id
-# }
