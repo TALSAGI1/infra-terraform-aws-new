38fc22d6bff3e04593f9435be333eaa3caea25af
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-25 16:53:25 +0300

    Update main.tf

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,15 +1,20 @@
-provider "aws" { region = var.region }
+provider "aws" { 
+region = var.region
+}
 
 resource "aws_s3_bucket" "tfstate" {
   bucket = var.bucket_name
 
   server_side_encryption_configuration {
-    rule {
+    rule 
+{
       apply_server_side_encryption_by_default { sse_algorithm = "AES256" }
     }
   }
 
-  versioning { enabled = true }
+  versioning { 
+enabled = true
+}
 
   tags = {
     Name      = var.bucket_name
@@ -21,7 +26,8 @@ resource "aws_dynamodb_table" "lock" {
   name         = var.dynamodb_table
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-  attribute {
+  attribute 
+{
      name = "LockID" 
      type = "S" 
      }
