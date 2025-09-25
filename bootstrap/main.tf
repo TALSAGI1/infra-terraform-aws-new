37062bddd0595b2cef6b27ed1ddfb0bb724e6182
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-25 16:55:25 +0300

    Update main.tf

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,20 +1,21 @@
-provider "aws" { 
-region = var.region
+provider "aws" {
+  region = var.region
 }
 
 resource "aws_s3_bucket" "tfstate" {
   bucket = var.bucket_name
 
   server_side_encryption_configuration {
-    rule 
-{
-      apply_server_side_encryption_by_default { sse_algorithm = "AES256" }
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
   }
 
-  versioning { 
-enabled = true
-}
+  versioning {
+    enabled = true
+  }
 
   tags = {
     Name      = var.bucket_name
@@ -26,11 +27,13 @@ resource "aws_dynamodb_table" "lock" {
   name         = var.dynamodb_table
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-  attribute 
-{
-     name = "LockID" 
-     type = "S" 
-     }
- 
-  tags = { ManagedBy = "terraform-bootstrap" }
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    ManagedBy = "terraform-bootstrap"
+  }
 }
