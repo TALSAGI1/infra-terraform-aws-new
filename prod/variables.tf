d6852ec956be0929f405ab4804b85e2cd73b922f
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-25 14:04:14 +0300

    Update variables.tf

--- prod/variables.tf
+++ prod/variables.tf
@@ -1,19 +1,34 @@
-variable "region"         { 
+variable "region"         {
 type = string 
 default = "eu-west-1" 
 }
-variable "vpc_cidr"       { 
-type = string 
+
+# רשת – ברירת־מחדל סבירה
+variable "vpc_cidr"       {
+type = string     
 default = "10.0.0.0/16"
 }
-variable "public_subnets" {
-type = list(string)
-default = ["10.0.1.0/24","10.0.2.0/24"] 
+variable "public_subnets" { 
+type = list(string)  
+default = ["10.0.1.0/24","10.0.2.0/24"]
 }
+variable "azs"            { 
+type = list(string)
+default = ["eu-west-1a","eu-west-1b"] 
+}
+
+# EC2 – ברירות־מחדל
 variable "instance_type"  { 
-type = string  
+type = string      
 default = "t3.micro"
 }
-variable "key_name"       { 
-type = string
-}  # שם ה-KeyPair שקיים ב-EC2
+variable "name"           {
+type = string     
+default = "prod-ec2" 
+}
+
+# פתיחת פורטים ב-SG (אם המודול SG צריך רשימה)
+variable "allowed_ports"  { 
+type = list(number)
+default = [22, 80, 443]
+}
