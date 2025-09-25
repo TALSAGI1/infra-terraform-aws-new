e50ab217f3ca4f8560467ce0100b36fd4d5b86a5
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-25 17:01:56 +0300

    Update variables.tf

--- prod/variables.tf
+++ prod/variables.tf
@@ -1,34 +1,37 @@
-variable "region"         {
-type = string 
-default = "eu-west-1" 
+variable "region" {
+  type    = string
+  default = "eu-west-1"
 }
 
 # רשת – ברירת־מחדל סבירה
-variable "vpc_cidr"       {
-type = string     
-default = "10.0.0.0/16"
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
 }
-variable "public_subnets" { 
-type = list(string)  
-default = ["10.0.1.0/24","10.0.2.0/24"]
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
-variable "azs"            { 
-type = list(string)
-default = ["eu-west-1a","eu-west-1b"] 
+
+variable "azs" {
+  type    = list(string)
+  default = ["eu-west-1a", "eu-west-1b"]
 }
 
 # EC2 – ברירות־מחדל
-variable "instance_type"  { 
-type = string      
-default = "t3.micro"
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
 }
-variable "name"           {
-type = string     
-default = "prod-ec2" 
+
+variable "name" {
+  type    = string
+  default = "prod-ec2"
 }
 
 # פתיחת פורטים ב-SG (אם המודול SG צריך רשימה)
-variable "allowed_ports"  { 
-type = list(number)
-default = [22, 80, 443]
+variable "allowed_ports" {
+  type    = list(number)
+  default = [22, 80, 443]
 }
